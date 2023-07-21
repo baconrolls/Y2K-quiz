21b8596b652f02d7c19e97f4f12137f89c50b1cf
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 13:58:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
 const quizData = [
-    // Add your questions and answers here
+    {
+        question: "What does Y2K stand for?",
+        options: ["Year 2000", "Youth 2 Knowledge", "Yesterday 2 Kitchens"],
+        answer: "Year 2000",
+    },
+    {
+        question: "What was the main concern of the Y2K bug?",
+        options: ["Economic collapse", "Computers malfunctioning", "Climate change"],
+        answer: "Computers malfunctioning",
+    },
+    {
+        question: "When did the Y2K bug occur?",
+        options: ["January 1, 1999", "December 31, 2000", "January 1, 2000"],
+        answer: "January 1, 2000",
+    },
+    {
+        question: "What is another name for the Y2K bug?",
+        options: ["Millennium bug", "Cyber bug", "Tech glitch"],
+        answer: "Millennium bug",
+    },
+    {
+        question: "Which programming language was most affected by the Y2K bug?",
+        options: ["C++", "Java", "COBOL"],
+        answer: "COBOL",
+    },
+    {
+        question: "How many digits are used to represent the year in the Y2K bug?",
+        options: ["4", "2", "3"],
+        answer: "2",
+    },
+    {
+        question: "What event did people fear might happen because of the Y2K bug?",
+        options: ["World War III", "Mass power outages", "Zombie apocalypse"],
+        answer: "Mass power outages",
+    },
+    {
+        question: "Which industry spent the most on Y2K bug fixes?",
+        options: ["Banking", "Healthcare", "Automotive"],
+        answer: "Banking",
+    },
+    {
+        question: "In which year did preparations for the Y2K bug start?",
+        options: ["1998", "1999", "2000"],
+        answer: "1998",
+    },
+    {
+        question: "Which country was the first to experience the new millennium?",
+        options: ["United States", "China", "New Zealand"],
+        answer: "New Zealand",
+    },
 ];
 
 const questionContainer = document.getElementById("question-container");

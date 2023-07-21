5acf8fb42f1ed4247e6e3bd549fe1d30dbd355df
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 18:25:04 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,73 +1,26 @@
-// Function to handle the result display
-function showResult(score) {
-  const resultContainer = document.getElementById("result-container");
-  const scoreText = document.getElementById("score");
-  scoreText.innerText = score;
-
-  // Hide the current question container and display the result container
-  const currentQuestionContainer = document.getElementById("question-container");
-  currentQuestionContainer.style.display = "none";
-  resultContainer.style.display = "block";
-}
-
-// Function to handle the click event of answer options
-function selectAnswer(answer, correct) {
-  if (correct) {
-    score++;
-  }
-  
-  const nextQuestion = questions[currentQuestion].next;
-  if (nextQuestion === "result") {
-    showResult(score);
-  } else {
-    displayQuestion(nextQuestion);
-  }
-}
-
-// Function to display the current question
-function displayQuestion(question) {
-  currentQuestion = question;
-  const questionContainer = document.getElementById("question-container");
-  questionContainer.innerHTML = `
-    <h1>${questions[question].question}</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Skincare Quiz</title>
+</head>
+<body>
+  <div id="question-container" class="container">
+    <h1>What is the first step of a skincare routine?</h1>
     <div class="options">
-      ${questions[question].answers
-        .map(
-          (answer, index) => `
-            <button onclick="selectAnswer(${index}, ${answer.correct})">
-              ${answer.text}
-            </button>
-          `
-        )
-        .join("")}
+      <button onclick="selectAnswer(0)">Toner</button>
+      <button onclick="selectAnswer(1)">Cleanser</button>
+      <button onclick="selectAnswer(2)">Moisturizer</button>
     </div>
-  `;
-}
+  </div>
 
-// Quiz questions
-const questions = {
-  question1: {
-    question: "What is the first step of a skincare routine?",
-    answers: [
-      { text: "Toner", correct: false },
-      { text: "Cleanser", correct: true },
-      { text: "Moisturizer", correct: false },
-    ],
-    next: "question2",
-  },
-  question2: {
-    question: "Which ingredient is known to help with acne?",
-    answers: [
-      { text: "Hyaluronic Acid", correct: false },
-      { text: "Niacinamide", correct: true },
-      { text: "Vitamin C", correct: false },
-    ],
-    next: "result",
-  },
-};
+  <div id="result-container" class="container" style="display: none;">
+    <h1>Your Score</h1>
+    <p id="score">0</p>
+  </div>
 
-let currentQuestion = "question1";
-let score = 0;
-
-// Start the quiz
-displayQuestion(currentQuestion);
+  <script src="quiz.js"></script>
+</body>
+</html>

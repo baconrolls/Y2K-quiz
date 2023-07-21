5a040fc203b7a95d8b7330e26eb0483043a69a8c
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 14:04:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const quizContainer = document.getElementById('quiz');
+const submitButton = document.getElementById('submit-btn');
+let score = 0;
+
 const quizData = [
     {
         question: "What does Y2K stand for?",
@@ -51,71 +55,50 @@ const quizData = [
     },
 ];
 
-const questionContainer = document.getElementById("question-container");
-const optionsContainer = document.getElementById("options-container");
-const resultContainer = document.getElementById("result-container");
-const scoreResult = document.getElementById("score-result");
-const expertise = document.getElementById("expertise");
-const expertiseDescription = document.getElementById("expertise-description");
+function buildQuiz() {
+    const output = [];
 
-let currentQuestion = 0;
-let score = 0;
+    questions.forEach((currentQuestion, questionNumber) => {
+        const options = [];
 
-function showQuestion() {
-    const currentQuizData = quizData[currentQuestion];
-    questionContainer.innerHTML = currentQuizData.question;
-    optionsContainer.innerHTML = "";
+        for (const option of currentQuestion.options) {
+            options.push(
+                `<label>
+                    <input type="radio" name="question${questionNumber}" value="${option}">
+                    ${option}
+                </label>`
+            );
+        }
 
-    currentQuizData.options.sort(() => Math.random() - 0.5); // Shuffle the options
-
-    currentQuizData.options.forEach((option) => {
-        const button = document.createElement("button");
-        button.innerHTML = option;
-        button.classList.add("option-btn");
-        button.addEventListener("click", () => selectAnswer(option));
-        optionsContainer.appendChild(button);
+        output.push(
+            `<div class="quiz-question">
+                <h2>${currentQuestion.question}</h2>
+                <div class="quiz-options">${options.join('')}</div>
+            </div>`
+        );
     });
+
+    quizContainer.innerHTML = output.join('');
 }
 
-function selectAnswer(selectedOption) {
-    const currentQuizData = quizData[currentQuestion];
+function showResults() {
+    const answerContainers = quizContainer.querySelectorAll('.quiz-options');
+    questions.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selectedOption = answerContainer.querySelector(`input[name="question${questionNumber}"]:checked`);
+        const userAnswer = selectedOption.value;
+        if (userAnswer === currentQuestion.answer) {
+            score++;
+            answerContainer.style.color = 'green';
+        } else {
+            answerContainer.style.color = 'red';
+        }
+    });
 
-    const buttons = document.getElementsByClassName("option-btn");
-    for (const button of buttons) {
-        button.disabled = true;
-    }
-
-    if (selectedOption === currentQuizData.answer) {
-        score++;
-    }
-
-    currentQuestion++;
-
-    if (currentQuestion < quizData.length) {
-        showQuestion();
-    } else {
-        showResult();
-    }
+    const result = score >= 8 ? "Congratulations! You're a Y2K expert!" : "Oops! You might need to brush up on your Y2K knowledge.";
+    const resultDisplay = `<div class="quiz-result">${result}</div>`;
+    quizContainer.insertAdjacentHTML('beforeend', resultDisplay);
 }
 
-function showResult() {
-    questionContainer.style.display = "none";
-    optionsContainer.style.display = "none";
-    resultContainer.style.display = "block";
-    scoreResult.textContent = `${score} / ${quizData.length}`;
-
-    let percentage = (score / quizData.length) * 100;
-
-    if (percentage >= 80) {
-        expertise.textContent = "You're a Y2K Expert!";
-        expertiseDescription.textContent = "You know everything there is to know about the Y2K bug. Well done!";
-    } else if (percentage >= 50) {
-        expertise.textContent = "You're Y2K Informed!";
-        expertiseDescription.textContent = "You have a good understanding of the Y2K bug, but there's still more to learn.";
-    } else {
-        expertise.textContent = "You're Y2K Curious!";
-        expertiseDescription.textContent = "You might not know much about the Y2K bug, but that's okay. It's a fascinating piece of history to explore!";
-    }
-}
-
-showQuestion();
+buildQuiz();
+submitButton.addEventListener('click', showResults);

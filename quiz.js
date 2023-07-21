c1ab2f6bc2b54fcfd23a863e13249a4ff79f3d07
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 17:49:23 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,52 +1,73 @@
-const questions = [
-  {
-    question: "What is the first step of a skincare routine?",
-    options: ["Cleansing", "Moisturizing", "Toning"],
-    answer: "Cleansing",
-  },
-  {
-    question: "What ingredient is known for its anti-aging properties?",
-    options: ["Hyaluronic Acid", "Retinol", "Tea Tree Oil"],
-    answer: "Retinol",
-  },
-  // Add more questions here...
-];
+// Function to handle the result display
+function showResult(score) {
+  const resultContainer = document.getElementById("result-container");
+  const scoreText = document.getElementById("score");
+  scoreText.innerText = score;
 
-let currentQuestion = 0;
-let score = 0;
-
-function loadQuestion() {
-  const questionText = document.getElementById("question");
-  const options = document.getElementById("options");
-
-  questionText.textContent = questions[currentQuestion].question;
-  options.innerHTML = "";
-
-  questions[currentQuestion].options.forEach((option) => {
-    const button = document.createElement("button");
-    button.textContent = option;
-    button.onclick = function() {
-      checkAnswer(option);
-    };
-    options.appendChild(button);
-  });
+  // Hide the current question container and display the result container
+  const currentQuestionContainer = document.getElementById("question-container");
+  currentQuestionContainer.style.display = "none";
+  resultContainer.style.display = "block";
 }
 
-function checkAnswer(selectedOption) {
-  const correctAnswer = questions[currentQuestion].answer;
-
-  if (selectedOption === correctAnswer) {
+// Function to handle the click event of answer options
+function selectAnswer(answer, correct) {
+  if (correct) {
     score++;
   }
-
-  currentQuestion++;
-
-  if (currentQuestion < questions.length) {
-    window.location.href = "question" + (currentQuestion + 1) + ".html";
+  
+  const nextQuestion = questions[currentQuestion].next;
+  if (nextQuestion === "result") {
+    showResult(score);
   } else {
-    localStorage.setItem("score", score);
-    window.location.href = "result.html";
+    displayQuestion(nextQuestion);
   }
 }
 
-loadQuestion();
+// Function to display the current question
+function displayQuestion(question) {
+  currentQuestion = question;
+  const questionContainer = document.getElementById("question-container");
+  questionContainer.innerHTML = `
+    <h1>${questions[question].question}</h1>
+    <div class="options">
+      ${questions[question].answers
+        .map(
+          (answer, index) => `
+            <button onclick="selectAnswer(${index}, ${answer.correct})">
+              ${answer.text}
+            </button>
+          `
+        )
+        .join("")}
+    </div>
+  `;
+}
+
+// Quiz questions
+const questions = {
+  question1: {
+    question: "What is the first step of a skincare routine?",
+    answers: [
+      { text: "Toner", correct: false },
+      { text: "Cleanser", correct: true },
+      { text: "Moisturizer", correct: false },
+    ],
+    next: "question2",
+  },
+  question2: {
+    question: "Which ingredient is known to help with acne?",
+    answers: [
+      { text: "Hyaluronic Acid", correct: false },
+      { text: "Niacinamide", correct: true },
+      { text: "Vitamin C", correct: false },
+    ],
+    next: "result",
+  },
+};
+
+let currentQuestion = "question1";
+let score = 0;
+
+// Start the quiz
+displayQuestion(currentQuestion);
